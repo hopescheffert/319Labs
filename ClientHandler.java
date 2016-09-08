ca4c8a4e40c93e379382ac733c9f6f231601d333
hopescheffert <hopees@iastate.edu>
2016-09-08 10:43:49 -0500

    working on sending the text message to server

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,6 +1,9 @@
 package messenger;
 
 import java.io.BufferedInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -18,7 +21,7 @@ class ClientHandler implements Runnable {
 	{
 		Scanner in;
 
-		// 1. USE THE SOCKET TO READ WHAT THE CLIENT IS SENDING
+		// 1. USE THE SOCKET TO READ WHAT THE CLIENT IS SENDING?
 		in = new Scanner(new BufferedInputStream(System.in));
 		//display menu to client
 		System.out.println("What would you like to do? "
@@ -26,14 +29,38 @@ class ClientHandler implements Runnable {
 		System.out.println("1. Send a text message to the server");
 		System.out.println("2. Send an image file to the server");
 		int choice = in.nextInt();
+		
 		if(choice == 1)
 		{
 			//TODO send text message to server
+			//TODO make sure chat goes to chat.txt
+			
+			
+			String clientTextMessage = "";
 			System.out.println("Please type your text message and press enter");
 			while(in.hasNextLine())
 			{
-				String clientTextMessage = in.nextLine();
+				clientTextMessage = in.nextLine();
 			}
+			//send to encryption method
+			byte[] encryptedMessage;
+			try 
+			{
+				//get encrypted message
+				encryptedMessage = encryptMessage(clientTextMessage);
+				//send it to server through the socket
+				DataOutputStream out = new DataOutputStream(s.getOutputStream());
+				out.writeInt(encryptedMessage.length); //write length of the message
+				out.write(encryptedMessage); //write the message
+				
+			} catch (UnsupportedEncodingException e) {
+				System.out.println("----Problem with encrypting the message");
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 
 		}
 		else if(choice == 2)
@@ -53,13 +80,35 @@ class ClientHandler implements Runnable {
 		// This handling code dies after doing all the printing
 	} // end of method run()
 
-	void printSocketInfo(Socket s) {
-		System.out.print("Socket on Server " + Thread.currentThread() + " ");
-		System.out.print("Server socket Local Address: " + s.getLocalAddress()
-		+ ":" + s.getLocalPort());
-		System.out.println("  Server socket Remote Address: "
-				+ s.getRemoteSocketAddress());
-	} // end of printSocketInfo
-
+	
+	/**
+	 * Method of Encryption:
+	 * For every byte Bn in a String, encrypted Byte = Bn XOR 11110000
+	 * e.g. Bn = 10101000, then Bn Xor 11110000 = 01011000
+	 * @throws UnsupportedEncodingException 
+	 */
+	public byte[] encryptMessage(String message) throws UnsupportedEncodingException
+	{
+		//charSet name?
+		byte[] encryptedBytes = message.getBytes();
+		for(int i = 0; i < encryptedBytes.length; i ++)
+		{
+			//do XOR with 11110000
+			encryptedBytes[i] = (byte) (encryptedBytes[i] ^ 11110000);
+		
+		}
+		return encryptedBytes;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 } // end of class ClientHandler
--- Server.java
+++ Server.java
@@ -1,7 +1,9 @@
 package messenger;
 
 import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -9,6 +11,8 @@ import java.util.Scanner;
 public class Server {
 
 	private static String clientName;
+	private static ServerSocket serverSocket;
+	private static Socket clientSocket;
 
 	public static String getUsername()
 	{
@@ -27,7 +31,7 @@ public class Server {
 		
 		
 
-		ServerSocket serverSocket = null;
+		serverSocket = null;
 		// 1. CREATE A NEW SERVERSOCKET
 		try 
 		{
@@ -41,7 +45,7 @@ public class Server {
 		// 2. LOOP FOREVER - SERVER IS ALWAYS WAITING TO PROVIDE SERVICE!
 		//Client.main(args);
 		while (true) {
-			Socket clientSocket = null;
+			clientSocket = null;
 
 			try {
 				// 2.1 WAIT FOR CLIENT TO TRY TO CONNECT TO SERVER
@@ -65,6 +69,46 @@ public class Server {
 
 
 	} // end of main method
+	
+	/**
+	 * Method of Decryption:
+	 * For every byte Bn in the received String, Let Bn  = Bn XOR 11110000
+	 * e.g. Bn = 01011000, then Bn Xor 11110000 = 10101000
+	 * @throws UnsupportedEncodingException 
+	 */
+	public byte[] decryptMessage(byte[] encryptedBytes) throws UnsupportedEncodingException
+	{
+		byte[] decryptedBytes = null;
+		
+		//for each of the encrypted bytes XOR and put into decryptedBytes
+		for(int i = 0; i < encryptedBytes.length; i ++)
+		{
+			//do XOR with 11110000
+			decryptedBytes[i] = (byte) (encryptedBytes[i] ^ 11110000);
+		
+		}
+		return decryptedBytes;
+	}
+	
+	public String getTextMessage(byte [] decryptedBytes) throws IOException
+	{
+		String message = "";
+		DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+		//the input stream will get the encrypted message from the client
+		
+		int length = in.readInt();
+		
+		if(length > 0)
+		{
+			byte[] msg = new byte[length]; //read length of incoming message
+			in.readFully(msg, 0, msg.length); //read the message
+			msg = decryptMessage(msg); //decrypt the message
+			//TODO see if this actually gets the original text message as a string
+			message = msg.toString();
+		}
+		return message;
+	}
+	
 }//end of Server class
 
 
