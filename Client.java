3d3211f543e2d2d37ca948600d69f832eba7e705
hopescheffert <hopees@iastate.edu>
2016-09-15 11:34:45 -0500

    new template

--- Client.java
+++ Client.java
@@ -2,11 +2,18 @@ package messenger;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -15,182 +22,256 @@ import java.util.Scanner;
 
 public class Client 
 {
-	
-	
-	private ObjectOutputStream streamOut = null;
+	private PrintWriter streamOut = null;
 	private static Socket serverSocket = null;
 	ClientListenForServerMessage listen;
-	ClientSendMessage sendmsg;
+	ClientSendMessageToServer sendmsg;
 	static String username;
 	private ArrayList<Client> clientList = new ArrayList<Client>();
+	private int choice;
+	private Scanner streamIn;
+
 
 	Client(String username) throws UnknownHostException, IOException
 	{
-		serverSocket = new Socket("localhost", 4444);
+		//CONNECT TO SERVER
+		serverSocket = new Socket("localhost", 1222);
 		this.username = username;
 		clientList.add(this);
-		
+
 		//SPAWN A LISTENER FOR THE SERVER. THIS WILL KEEP RUNNING
 		listen = new ClientListenForServerMessage(this, serverSocket);
-		new Thread(listen).start();
+//		new Thread(listen).start();
 		
+		//DISPLAY MENU TO CLIENT
+		System.out.println("What would you like to do? "
+				+ "Please choose 1 or 2 and press enter:");
+		System.out.println("1. Send a text message to the server");
+		System.out.println("2. Send an image file to the server");
+		Scanner in = new Scanner(System.in);
+		streamOut = new PrintWriter(new BufferedOutputStream(serverSocket.getOutputStream()));
+		
+		choice = in.nextInt();
+		handleChoice(choice);
+		//tell the server your choice
+		streamOut.println(choice);
+		streamOut.flush();
+	}
+	
+	public void handleChoice(int choice) throws IOException
+	{
+		streamIn = new Scanner(new BufferedInputStream(serverSocket.getInputStream()));
+
+		if(choice == 1) //wants to send a message
+		{
+			streamOut.println("Please type your message");
+			streamOut.flush();
+			
+
+			//TODO make sure message goes to chat.txt
+			handle(streamIn.nextLine());
+		}
+
+
+
+
+		//****IMAGE*****			
+
+		else if(choice == 2) //wants to send an image
+		{
+			//TODO send an image file to the server using java's object output stream
+			try {
+				ObjectOutputStream oos = new ObjectOutputStream(serverSocket.getOutputStream());
+
+				//Object ob = new Object();
+				//oos.writeObject(ob);
+
+				//stuff here
+
+				//oos.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			//ask client to enter the file name of the image
+			System.out.println("Please enter your image file name and press enter");
+			Scanner img = new Scanner(System.in);
+			String imgpath = img.nextLine();
+			img.close();
+			//TODO convert image to a string sequence and encrypt that
+			//send it to encryption method
+			//byte[] encryptedImage = encryptImage(imgpath);
+
+
+			//get image from the client
+			try 
+			{
+				ObjectInputStream ois = new ObjectInputStream(serverSocket.getInputStream());
+				//Object ob1 = ois.readObject();
+
+				//stuff here
+
+				//ois.close();
+
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+		else
+		{ System.out.println("Error: Please enter either 1 or 2");}
 	}
 
 	public static String getUsername()
 	{
 		return username;
 	}
-	
-	public void handleMessage(String msg)
+
+	public void handle(String msg) throws IOException
 	{
-		//now spawn a thread to deal with client send message
-		//***don't know if this should go here or in client handler???***
-		//PrintWriter out;
-		//out = new PrintWriter(new BufferedOutputStream(serverSocket.getOutputStream()));
-		ClientSendMessage csm = new ClientSendMessage(serverSocket, username);
-		csm.go();
+		//System.out.println(msg);
+
+		//write the message to chat.txt
+		String encrypted = encryptMessage(msg);
+		streamOut.println(encrypted);
+		streamOut.flush();
+		File f = new File("chat.txt");
+		FileWriter fout = new FileWriter(f);
+		fout.write(username + ": " + encrypted + "\n");
+		fout.flush();
+		//fout.close();
+	}
+	
+	//broadcasts messages to all clients in the clientList
+	public synchronized void broadcast(String message) throws IOException
+	{
+		for(Client c : clientList)
+		{
+			c.handle(message);
+		}
 	}
 
 
 	public static void main(String[] args) throws UnknownHostException, IOException 
 	{
-		
 		//create scanner for client input
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter your name and press enter");
 		username = scanner.next();
-		
-		Client c = new Client(username);
-		
-		Scanner console;
-		PrintWriter out;
-
-		try
+		new Client(username);	
+		//check if they are admin: have special menu
+		if(username.equals("admin"))
 		{
-			// 1. USE THE SOCKET TO READ WHAT THE CLIENT IS SENDING?
-			//in = new Scanner(new BufferedInputStream(s.getInputStream())); 
-			console = new Scanner(new BufferedInputStream(System.in)); 
-			out = new PrintWriter(new BufferedOutputStream(serverSocket.getOutputStream()));
-			//out = new PrintWriter(s.getOutputStream());
-			//display menu to client
-			System.out.println("What would you like to do? "
-					+ "Please choose 1 or 2 and press enter:");
-			System.out.println("1. Send a text message to the server");
-			System.out.println("2. Send an image file to the server");
+			System.out.println("What would you like to do? Please choose 1, 2, or 3.");
+			System.out.println("1. Brodcast message to all clients");
+			System.out.println("2. List messages so far");
+			System.out.println("3. Delete a specified message (please give a message number)");
 
-			//keep listening and responding to client requests
-
-			int choice = console.nextInt();
-
-			if(choice == 1) //wants to send a message
+			int choice = scanner.nextInt();
+			if(choice == 1)
 			{
-				//TODO make sure message goes to chat.txt
-
-				//send to a new thread to deal with client sending message
-				ClientSendMessage csm = new ClientSendMessage(serverSocket, username);
-				csm.go();
+				//TODO broadcast message to all clients in clientList
+				//broadcast(scanner.nextLine());
 			}
-			
-			
-			//****IMAGE*****			
-			
-			else if(choice == 2) //wants to send an image
+			else if(choice == 2)
 			{
-				//TODO will look something like this:
-				//Thread t = new Thread(new ClientSendImage(s));
-				//t.start();
-				//TODO put this in new class called ClientSendImage implements Runnable
-				//TODO send an image file to the server using java's object output stream
-				try {
-					ObjectOutputStream oos = new ObjectOutputStream(serverSocket.getOutputStream());
-
-					//Object ob = new Object();
-					//oos.writeObject(ob);
-
-					//stuff here
-
-					//oos.close();
-
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-				//ask client to enter the file name of the image
-				System.out.println("Please enter your image file name and press enter");
-				Scanner img = new Scanner(System.in);
-				String imgpath = img.nextLine();
-				img.close();
-				//TODO convert image to a string sequence and encrypt that
-				//send it to encryption method
-				//byte[] encryptedImage = encryptImage(imgpath);
-
-
-				//get image from the client
-				try 
-				{
-					ObjectInputStream ois = new ObjectInputStream(serverSocket.getInputStream());
-					//Object ob1 = ois.readObject();
-
-					//stuff here
-
-					//ois.close();
-
-
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
+				//TODO list messages so far from chat.txt
+				//getMessagesSoFar();
 			}
-			else
-			{ System.out.println("Please enter either 1 or 2");}
+			else if(choice == 3)
+			{
+				//TODO delete specified message (get message number from scanner)
+				//int messageNum = scanner.nextInt();
+			}
+			else System.out.println("Please choose either 1, 2, or 3");
+		}
+		//scanner.close();
 
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
-		// This handling code dies after doing all the printing
-		
-		
-		
-		//spawn new thread to deal with server handling the message from the client
-		Thread t2 = new Thread(new GetMessageFromClient(serverSocket, c.username));
-		t2.start();
-		
 		while(true)
 		{
 			//keeps client running forever
-		}
 
+		}
 
 	}
 	
-	/**
-	 * IMAGE
-	 * Method of Encryption:
-	 * Suppose an image is stored by m bytes
-	 * 
-	 * For every 3 bytes, we have 24 bits
-	 * 		Divide 24 into four 6 bit fragments
-	 * 		Add two bits of 00 to the right of each 6 bits fragment
-	 * 		(then we can get four 8 bit fragments)
-	 * 		Convert the 8 bit fragments into characters and combine them to make a string
-	 * 
-	 * @param path
-	 * @return
-	 */
-	public byte[] encryptImage(String path)
+	//get messages from chat.txt
+	public synchronized void getMessagesSoFar() throws FileNotFoundException
 	{
-		byte[] encryptedBytes = path.getBytes();
-		int len = encryptedBytes.length;
-		//for every 3 bytes
-
-		for(int i = 0; i < len % 3; i++)
+		File f = new File("chat.txt");
+		Scanner file = new Scanner(f);
+		String messages = "";
+		while(file.hasNext())
 		{
-			//TODO encrypt!
+			messages += file.nextLine();
+		}
+		streamOut.print(messages);
+		
+	}
 
+	
+	/**
+	 * MESSAGE
+	 * Gets the client's text message and outputs an encrypted byte array 
+	 * Method of Encryption:
+	 * For every byte Bn in a String, encrypted Byte = Bn XOR 11110000
+	 * e.g. Bn = 10101000, then Bn Xor 11110000 = 01011000
+	 * @throws UnsupportedEncodingException 
+	 */
+	public String encryptMessage(String message) throws UnsupportedEncodingException
+	{
+		byte[] encryptedBytes = message.getBytes();
+		//TODO change to ascii values then encrypt
+		//int ascii = (int) character
+		for(int i = 0; i < encryptedBytes.length; i ++)
+		{
+			//do XOR with 11110000
+			encryptedBytes[i] = (byte) (encryptedBytes[i] ^ 11110000);
 		}
 
-		return encryptedBytes;
+		return new String(encryptedBytes);
+	}
+
+}
+
+class ClientListenForServerMessage implements Runnable 
+{
+	Client c;
+	Scanner in; //blocking call
+	Socket s;
+
+
+	ClientListenForServerMessage(Client c, Socket s) throws IOException
+	{
+		this.c = c;
+		//scanner for output of the server
+//		in = new Scanner(new BufferedInputStream(s.getInputStream()));
+		this.s = s;
+	}
+
+	/**
+	 * Sole purpose is to listen for a message from the server at all times
+	 */
+	@Override
+	public void run()
+	{
+		
+		while(true) //run forever
+		{
+			try 
+			{
+				c.handle(in.nextLine());
+			} 
+			catch (UnsupportedEncodingException | FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 
 	}
+
 }
--- Server.java
+++ Server.java
@@ -1,8 +1,13 @@
 package messenger;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,36 +19,36 @@ public class Server {
 	private static ServerSocket serverSocket;
 	private static Socket clientSocket = null;
 
-	
 	public Server() {}
 
 
-	public static void main(String[] args) throws IOException 
+	public static void main(String[] args) throws IOException, InterruptedException 
 	{
 		serverSocket = null;
 		// 1. CREATE A NEW SERVERSOCKET
 		try 
 		{
-			serverSocket = new ServerSocket(4444); // provide MYSERVICE at port 4444
-		} catch (IOException e) {
-			System.out.println("Could not listen on port: 4444");
+			serverSocket = new ServerSocket(1222); // provide MYSERVICE at port 4444
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("Could not listen on port: 1222");
 			System.exit(-1);
 		}
 
-
 		// 2. LOOP FOREVER - SERVER IS ALWAYS WAITING TO PROVIDE SERVICE!
-
-		while (true) {
-
+		while (true) 
+		{
 			try {
 				// 2.1 WAIT FOR CLIENT TO TRY TO CONNECT TO SERVER
 				clientSocket = serverSocket.accept();
 
 				// 2.2 SPAWN A THREAD TO HANDLE CLIENT
-				//System.out.println("Server is connected to " + clientName);
-				//Thread t = new Thread(new ClientHandler(clientSocket, clientName));
-				Thread t = new Thread(new ServerListenForClientMessage(new Client(Client.getUsername()), clientSocket));
+				System.out.println("Server is connected to a client");
+
+				Thread t = new Thread(new ServerHandleClient(clientSocket, Client.username));
 				t.start();
+
 			} catch (IOException e) {
 				System.out.println("Accept failed: 4444");
 				System.exit(-1);
@@ -54,12 +59,134 @@ public class Server {
 	} // end of main method
 
 
-	
-
-
-
-
 }//end of Server class
 
+class ServerHandleClient implements Runnable 
+{
+	private static Socket s; // this is socket on the server side that connects to the CLIENT
+	private String username;
+	private int choice;
+
+	private Scanner  streamIn  =  null;
+	private PrintWriter streamOut = null;
+
+	ServerHandleClient(Socket s, String username) throws IOException, InterruptedException
+	{
+		this.s = s;
+		this.username = username;
+	}
+
+	//handle given message
+	public synchronized void handle(String input, int choice) throws IOException
+	{
+		if(choice == 1) //incoming encrypted message
+		{
+			try 
+			{
+				//get encrypted message
+				String encryptedMessage = decryptMessage(input);
+				//send to server
+				streamOut.println(encryptedMessage.length()); //write length of the message (an int)
+				streamOut.println(encryptedMessage); //write the message
+				streamOut.flush(); //forces output
+			} 
+			catch (UnsupportedEncodingException e) 
+			{
+				System.out.println("----Problem with encrypting the message");
+				e.printStackTrace();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+
+		}
+		else //incoming encrypted image
+		{
+
+		}
 
 
+	}
+
+	/**
+	 * Method of Decryption:
+	 * For every byte Bn in the received String, Let Bn  = Bn XOR 11110000
+	 * e.g. Bn = 01011000, then Bn Xor 11110000 = 10101000
+	 * @throws UnsupportedEncodingException 
+	 */
+	public synchronized String decryptMessage(String message) throws UnsupportedEncodingException
+	{
+		byte [] encryptedBytes = message.getBytes();
+		byte[] decryptedBytes = new byte[encryptedBytes.length];
+
+		//for each of the encrypted bytes XOR and put into decryptedBytes
+		for(int i = 0; i < encryptedBytes.length; i ++)
+		{
+			//do XOR with 11110000
+			decryptedBytes[i] = (byte) (encryptedBytes[i] ^ 11110000);
+
+		}
+		return new String(decryptedBytes);
+	}
+
+
+	//This is the client handling code
+	//This keeps running handling client requests 
+	@Override
+	public void run() 
+	{
+		try {
+			streamIn = new Scanner(new BufferedInputStream(s.getInputStream()));
+			//System.out.println("error");
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		while(true)
+		{
+			//get the choice - if its 1 handle message if its 2 handle image
+			choice = streamIn.nextInt();
+			try 
+			{
+				handle(streamIn.nextLine(), choice);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+	}// end of method run()
+
+	/**
+	 * IMAGE
+	 * Method of Encryption:
+	 * Suppose an image is stored by m bytes
+	 * 
+	 * For every 3 bytes, we have 24 bits
+	 * 		Divide 24 into four 6 bit fragments
+	 * 		Add two bits of 00 to the right of each 6 bits fragment
+	 * 		(then we can get four 8 bit fragments)
+	 * 		Convert the 8 bit fragments into characters and combine them to make a string
+	 * 
+	 * @param path
+	 * @return
+	 */
+	public byte[] encryptImage(String path)
+	{
+		byte[] encryptedBytes = path.getBytes();
+		int len = encryptedBytes.length;
+		//for every 3 bytes
+
+		for(int i = 0; i < len % 3; i++)
+		{
+			//TODO encrypt!
+		}
+
+		return encryptedBytes;
+
+	}
+
+
+} // end of class ClientHandler
