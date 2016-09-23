2a0dbb5ae6f6318abceac2eefeac26ae862d592a
hopescheffert <hopees@iastate.edu>
2016-09-23 14:16:05 -0500

    lab 3 template

--- calculator.js
+++ calculator.js
@@ -4,13 +4,17 @@
 //
 
 // 
-var Calc = {
+var Calc = 
+{
 
-Model : {
+Model : 
+{
+	
 },
 
 
-View : {
+View : 
+{
   textRow : {id: "textRow", type: "text", value: "", onclick:""},
   
   button7 : {id: "button7", type: "button", value: 7, onclick:""},
@@ -44,21 +48,25 @@ View : {
   
   },
 
-Controller : {
+Controller : 
+{
+	//TODO buttonHandler : function(that) { }
 	
 	
 	
 
 },
 
-run : function() {
+run : function() 
+{
   Calc.attachHandlers();
   console.log(Calc.display());
   return Calc.display();
 },
 
 
-displayElement : function (element) {
+displayElement : function (element) 
+{
   var s = "<input ";
   s += " id=\"" + element.id + "\"";
   s += " type=\"" + element.type + "\"";
@@ -69,7 +77,8 @@ displayElement : function (element) {
 
 },
 
-display : function() {
+display : function() 
+{
   var s;
   s = "<table id=\"myTable\" border=2>"
   s += "<tr><td>" + Calc.displayElement(Calc.View.textRow) + "</td></tr>";
@@ -80,12 +89,119 @@ display : function() {
   return s;
 },
 
-attachHandlers : function() {
+attachHandlers : function() 
+{
+	//TODO ATTACH HANDLER FOR EACH BUTTON HANDLER
   Calc.View.button7.onclick = "Calc.button7Handler()"; 
+  Calc.View.button6.onclick = "Calc.button6Handler()";
+  Calc.View.button5.onclick = "Calc.button5Handler()";
+  Calc.View.button4.onclick = "Calc.button4Handler()";
+  Calc.View.button3.onclick = "Calc.button3Handler()";
+  Calc.View.button2.onclick = "Calc.button2Handler()";
+  Calc.View.button1.onclick = "Calc.button1Handler()";
+  Calc.View.button0.onclick = "Calc.button0Handler()";
+  Calc.View.button8.onclick = "Calc.button8Handler()";
+  Calc.View.buttonC.onclick = "Calc.buttonCHandler()";
+  
+
 },
 
-button7Handler : function() {
+//BUTTON HANDLER FOR EACH BUTTON
+
+
+buttonCHandler : function()
+{
+	//clears screen value
+	Calc.view.textView.value = "";
+}
+
+buttonPlusHandler : function()
+{
+	//TODO puts + on textView and adds whats before and after it
+}
+
+button0Handler : function()
+{
+	//TODO
+}
+button1Handler : function()
+{
+	//TODO
+}
+button2Handler : function()
+{
+	//TODO
+}
+button3Handler : function()
+{
+	//TODO
+}
+button4Handler : function()
+{
+	//TODO
+}
+button5Handler : function()
+{
+	//TODO
+}
+button6Handler : function()
+{
+	//TODO
+}
+button7Handler : function() 
+{
   //TODO
 }
+button8Handler : function()
+{
+	//TODO
+}
+button9Handler : function()
+{
+	//TODO
+}
+buttonDivHandler : function()
+{
+	//TODO
+}
+buttonMultHandler : function()
+{
+	//TODO
+}
+buttonEqualsHandler : function()
+{
+	//TODO
+}
+buttonMRHandler : function()
+{
+	//TODO
+}
+buttonM-Handler : function()
+{
+	//TODO
+}
+buttonM+Handler : function()
+{
+	//TODO
+}
+buttonMCHandler : function()
+{
+	//TODO
+}
+buttonDotHandler : function()
+{
+	//TODO
+}
+
+
+
+
+
+
+
+
+
+
+
 
 } // end of Calc;
