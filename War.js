2fab2530a578ab9297bcf7e2c8f7f98b712ebc95
hopescheffert <hopees@iastate.edu>
2016-09-27 13:01:10 -0500

    working

--- War.js
+++ War.js
@@ -1,7 +1,5 @@
 //War.JS
 
-//http://www.brainjar.com/js/cards/default2.asp
-
 var War =
 {
 
@@ -9,7 +7,7 @@ var War =
 	{
 		//create a card object
 		//Ex. value="2", suit= "c" for 2 of Clubs
-		makeCard : function(val, suit)//, img)
+		makeCard : function(val, suit)
 		{
 			this.val = val;
 			this.suit = suit;
@@ -42,10 +40,9 @@ var War =
 		shuffleCards : function(cards)
 		{
 			//shuffle 3 times
-			for(var i = 0; i < 2; i++)
+			for(var i = 0; i < 3; i++)
 			{
-				//TODO FIX FROM TESTING go through cards and randomly switch around
-				for(var j = 1; j < cards.length; j++)
+				for(var j = 0; j < cards.length; j++)
 				{
 					var r = Math.floor(Math.random() * cards.length);
 					var temp = cards[j];
@@ -87,79 +84,103 @@ var War =
 
 	View :
 	{
-		startButton : {id: "startButton", type: "button", value: "Start Game", onclick:""},
+		//buttons
 		quitButton : {id: "quitButton", type: "button", value: "Quit Game", onclick:""},
-		takeTurnButton : {id: "takeTurnButton", type: "button", value: "Lay Down A Card", onclick:""},
-		playerDeck : {id: "playerDeck", src: "/Users/hopescheffert/Documents/COMS319/WarGame/cards/back.gif"},
-		playerCardUp : {id: "playerCardUp", src: ""},
-		computerDeck : {id: "computerDeck", src: "/Users/hopescheffert/Documents/COMS319/WarGame/cards/back.gif"},
-		computerCardUp : {id: "computerCardUp", src: ""},
+		layDownACard : {id: "layDownACard", type: "button", value: "Lay Down Your Card", onclick:""},
+		warLayDownButton : {id: "warLayDownButton", type: "button", value: "Go To War!", onclick:""},
+		//cards
+		playerDeck : {id: "playerDeck", src: "back"},
+		playerCard : {id: "playerCard", src: ""},
+		computerDeck : {id: "computerDeck", src: "back"},
+		computerCard : {id: "computerCard", src: ""},
+
+		//score
+		playerScore : {id:"playerScore", value:"Player Score is: "},
+		computerScore : {id:"computerScore", value:"Computer Score is: "},
+
 
 	},
 
 	Controller :
 	{
-		//onclick for start button to begin the game
-		startButtonHandler : function()
-		{
-			//makes deck and shuffles the cards
-			deck = War.Model.makeDeck();
-			deck = War.Model.shuffleCards(deck);
-
-			//deal cards
-			War.Model.dealPlayers(deck);
-			console.log("computerHand is " + computerHand);
-			console.log("playerHand is " + playerHand);
-			//TODO do we need to remove cards from deck?
-			//display score to begin with
-			War.Controller.getScore(playerHand, computerHand);
-
-		},
 		//onclick for quit button to end/restart the game
 		quitButtonHandler : function()
 		{
-			//TODO stops game...resets everything?
+			alert("Sorry to see you go!");
+			//restart/reset
+			War.startGame();
+			//hide the cards
+			playerCard.style.display = "none";
+			computerCard.style.display = "none";
 		},
 		//onclick for taking turn button aka laying down a card to be compared
-		takeTurnButtonHandler: function(playerHand, computerHand)
+		layDownACardHandler: function(playerHand, computerHand, inWarMode)
 		{
+			layDownACard.value = "Lay Down Your Card";
+
+
+			console.log("***in warLayDownButton cardsInPlay is " + cardsInPlay);
+
+
+			console.log("----in take turn---- and inWarMode is " + inWarMode);
+
 			//lay one card from hand face up at the same time
 			if(playerHand.length == 0)
 			{
 				//TODO in this case, the computer wins...game over
-				alert("game is over, you ran out of cards!");
+				alert("Sorry, you lose. Game is over, you ran out of cards!");
 			}
 			if(computerHand.length == 0)
 			{
 				//TODO in this case, the player wins...game over
-				alert("game is over, they ran out of cards!")
+				alert("YOU WIN! Game is over, they ran out of cards!")
 			}
 
+			//in war mode, we need to lay down our first card facing down
+			//then call take turn again and lay face up
+			if(inWarMode)
+			{
+				playerLayUp = cardsInPlay[0];
+				computerLayUp = cardsInPlay[1];
+				warLayDownButton.style.display = "block";
+				layDownACard.style.display = "none";
+				//make the on click method of "War" button
+				War.View["warLayDownButton"].onclick =
+				"War.Controller.warLayDownButtonHandler(playerHand, playerLayUp, " +
+					+ "computerHand, computerLayUp, cardsInPlay)";
+				return;
+			}
+
+
 			//PLAYER
-			var cardsInPlay = [];
-			//lay one card face up
-			var playerLayUp = playerHand.shift();
+
+			//get one card face up
+			playerLayUp = playerHand.shift();
 			//push to array cause the card is in play
 			cardsInPlay.push(playerLayUp);
 			console.log("not war: player lay up is " + playerLayUp);
 
-
-			//TODO display this card on the board?
-			War.View["playerCardUp"].src = "/Users/hopescheffert/Documents/COMS319/WarGame/cards/" + playerLayUp + ".gif";
-			var card = document.createElement("div");
-			console.log(War.displayCardElement(playerCardUp));
-			War.displayCardElement(playerCardUp);
-		    //card.innerHTML = "<img src=/Users/hopescheffert/Documents/COMS319/WarGame/cards/" + playerLayUp + ".gif height=102 width=73</img>";
-
 			//COMPUTER
-			//lay one card face up
-			var computerLayUp = computerHand.shift();
+			//get one card face up
+			computerLayUp = computerHand.shift();
 			//push to array cause the card is in play
 			cardsInPlay.push(computerLayUp);
 			console.log("not war: computer lay up is "  + computerLayUp);
-			//TODO display this card on the board?
-			//computerCardUp = War.displayCardElement(computerLayUp);
 
+
+
+
+
+			//lay down card FRONT like normal
+			War.View.playerCard.src = playerLayUp;
+			console.log(War.displayCardElement(War.View.playerCard));
+			War.Controller.layCard("pCard", War.View.playerCard);
+
+			War.View.computerCard.src = computerLayUp;
+			console.log(War.displayCardElement(War.View.computerCard));
+			War.Controller.layCard("cCard", War.View.computerCard);
+
+			//COMPARISON
 
 			var pval = Number(playerLayUp.substring(0,1));
 			if(pval == 1) pval = Number(playerLayUp.substring(0,2));
@@ -174,7 +195,7 @@ var War =
 			//these cards now need to be compared
 			if(pval > cval)
 			{
-				//TODO player wins this round and takes both cards
+				//player wins this round and takes both cards
 				console.log("cardsinplay is " + cardsInPlay);
 				var len = cardsInPlay.length;
 
@@ -183,14 +204,15 @@ var War =
 					playerHand.push(cardsInPlay.shift());
 				}
 				console.log("not war: player wins round..hand is " + playerHand);
-
+				document.getElementById("status").innerHTML = War.displayDivElement("You win this round!");
+				document.getElementById("status").style.color = "green";
 				War.Controller.getScore(playerHand, computerHand);
 
 			}
 			else if(pval < cval)
 			{
 
-				//TODO computer wins this round and takes both cards
+				//computer wins this round and takes both cards
 				console.log("cardsinplay is " + cardsInPlay);
 				var len = cardsInPlay.length;
 
@@ -199,171 +221,114 @@ var War =
 					computerHand.push(cardsInPlay.shift());
 				}
 				console.log("not war: computer wins round..hand is " + computerHand);
+				document.getElementById("status").innerHTML = War.displayDivElement("Computer wins this round");
+				document.getElementById("status").style.color = "red";
+
 
 				War.Controller.getScore(playerHand, computerHand);
 
 			}
 			else //they match...WAR
 			{
-				//TODO WAR
-				console.log("war needs to be called..cards in play is " + cardsInPlay);
-				War.Controller.war(playerHand, computerHand, cardsInPlay);
-			}
 
+				warMode = true;
+				console.log("war needs to be called..cards in play is " + cardsInPlay);
+				document.getElementById("status").innerHTML = War.displayDivElement("This is war!");
+				document.getElementById("status").style.color = "blue";
+
+				//handle war with take turn button in war mode
+				War.View["layDownACard"].onclick = War.Controller.layDownACardHandler(playerHand, computerHand, warMode);
+			}
 
 		},
-		war : function(playerHand, computerHand, cardsInPlay)
+		warLayDownButtonHandler : function(playerHand, playerLayUp, computerHand, computerLayUp, cardsInPlay)
 		{
-			//in war: each player lays down one card face down and one card face up
-			//check end cases
-			if(playerHand.length == 0)
-			{
-				//TODO in this case, the computer wins...game over
-				alert("game is over, you ran out of cards!");
+			//lays both cards face down when user clicks the war button
+			War.View.playerCard.src = "back";
+			//lay card down
+			War.Controller.layCard("pCard", War.View.playerCard);
+			console.log("**in warlaydown playerCard is " + playerLayUp);
+			//makes sure to push these cards into cardsInPlay
+			cardsInPlay.push(playerHand.shift());
 
-			}
-			else if(playerHand == 1)
-			{
-				//TODO in this case, they cannot layDown, just layUp
+			//set src to back of card img
+			War.View.computerCard.src = "back";
+			//lay card down
+			War.Controller.layCard("cCard", War.View.computerCard);
+			console.log("**in warlaydown computerCard is " + computerLayUp);
+			//makes sure to push these cards into cardsInPlay
+			cardsInPlay.push(computerHand.shift());
 
-			}
-			if(computerHand.length == 0)
-			{
-				//TODO in this case, the player wins...game over
-				alert("game is over, they ran out of cards!");
+			console.log("***in warLayDownButton cardsInPlay is " + cardsInPlay);
 
-			}
-			else if(computerHand == 1)
-			{
-				//TODO in this case, they cannot layDown, just layUp
-				//so call take turn button
-				War.Controller.takeTurnButton(playerHand, computerHand);
-			}
-
-			//PLAYER
-			//lay one card face down
-			var playerLayDown = playerHand.shift();
-			cardsInPlay.push(playerLayDown);
-			console.log("war: player lay down is " + playerLayDown);
-
-			//TODO display this card on the board?
-			//playerCardDown = War.displayCardElement(playerLayDown);
-
-			//lay one card face up
-			var playerLayUp = playerHand.shift();
-			cardsInPlay.push(playerLayUp);
-			console.log("war: player lay up is " + playerLayUp);
-
-			//TODO display this card on the board?
-			//playerCardUp = War.displayCardElement(playerLayUp);
-
-			//COMPUTER
-			//lay one card face down
-			var computerLayDown = computerHand.shift();
-			cardsInPlay.push(computerLayDown);
-			console.log("war: computer lay down is " + computerLayDown);
-
-			//TODO display this card on the board?
-			//computerCardDown = War.displayCardElement(computerLayDown);
-
-			//lay one card face up
-			var computerLayUp = computerHand.shift();
-			cardsInPlay.push(computerLayUp);
-			console.log("war: computer lay up is " + computerLayUp);
-
-			//TODO display this card on the board?
-			//computerCardUp = War.displayCardElement(computerLayUp);
-
-
-			var pval = Number(playerLayUp.substring(0,1));
-			if(pval == 1) pval = Number(playerLayUp.substring(0,2));
-			var cval = Number(computerLayUp.substring(0,1));
-			if(cval == 1) cval = Number(computerLayUp.substring(0,2));
-
-			//this layUp card now needs to be compared with opponents layUp card
-			if(pval > cval)
-			{
-
-				//TODO player wins this round and takes all cards from war
-				var len = cardsInPlay.length;
-
-				for(var i = 0; i < len; i++)
-				{
-					playerHand.push(cardsInPlay.shift());
-				}
-				console.log("war: player wins round with hand " + playerHand);
-
-				console.log("cardsinplay is " + cardsInPlay);
-
-				War.Controller.getScore(playerHand, computerHand);
-
-			}
-			else if(pval < cval)
-			{
-
-				//TODO computer wins this round and takes all cards from war
-				var len = cardsInPlay.length;
-				for(var i = 0; i < len; i++)
-				{
-					computerHand.push(cardsInPlay.shift());
-				}
-				console.log("war: computer wins round with hand " + computerHand);
-
-				console.log("cardsinplay is " + cardsInPlay);
-
-				War.Controller.getScore(playerHand, computerHand);
-
-			}
-			else
-			{
-				//TODO WAR
-				console.log("war: war again");
-
-				War.Controller.war(playerHand, computerHand, cardsInPlay);
-			}
-
-
+			//don't show war button, show lay down a card button
+			warLayDownButton.style.display = "none";
+			layDownACard.style.display = "block";
+			layDownACard.value = "Go to War!";
 
 		},
 		getScore : function(playerHand, computerHand)
 		{
-			//show player score and computer score
-			document.getElementById("playerScore").innerHTML = playerHand.length;
-			document.getElementById("computerScore").innerHTML = computerHand.length;
+			//update player score and computer score
+			playerScore.innerHTML = playerHand.length;
+			computerScore.innerHTML = computerHand.length;
 
-		}
+		},
+		layCard : function(id, cardDiv)
+		{
+			document.getElementById(id).innerHTML = War.displayCardElement(cardDiv);
+		},
+
 
 	},
 
 	run : function()
 	{
+		//create this array to keep track of cards in play
+		cardsInPlay = [];
+		War.startGame();
 		War.attachHandlers();
-		console.log(War.displayAll());
 		return War.displayAll();
 	},
+	//begin the game
+	startGame : function()
+	{
+		//makes deck and shuffles the cards
+		deck = War.Model.makeDeck();
+		deck = War.Model.shuffleCards(deck);
 
+		//deal cards
+		War.Model.dealPlayers(deck);
+		console.log("computerHand is " + computerHand);
+		console.log("playerHand is " + playerHand);
+		//TODO do we need to remove cards from deck?
+		//display score to begin with
+		War.Controller.getScore(playerHand, computerHand);
+
+	},
 	displayAll : function()
 	{
-		var s= "<h1>Welcome to War!<h1>";
-		s += War.displayInputElement(War.View.startButton);
-		s+= "<br>";
-		s += War.displayInputElement(War.View.takeTurnButton);
-		s+= "<br>";
+		var s= "<br>";
+		s += War.displayInputElement(War.View.layDownACard);
+		s += War.displayWarButton(War.View.warLayDownButton);
+		s += "<br>";
+		s += "<br><h5>Your Deck</h5>";
+		s += War.displayCardElement(War.View.playerDeck) + "				";
+		s += "<div style='display: inline' id='pCard' height=102 width=73></div>";
+		s += "<br><h5>Computer Deck</h5>";
+		s += War.displayCardElement(War.View.computerDeck) + "				";
+		s += "<div style='display: inline' id='cCard' height=102 width=73></div> <br>";
 		s += War.displayInputElement(War.View.quitButton);
-		s+= "<br>";
-		s += War.displayCardElement(War.View.playerDeck);
-		// s += War.displayCardElement(War.View.playerCardUp);
 		return s;
 	},
 	//displaying card
 	displayCardElement : function (element)
 	{
-		var s = "<br><br><img ";
+		var s = "<img style='display: inline' ";
 		s += " id=\"" + element.id + "\"";
-		//TODO why wont it show other elements with diff cards 
-		s += " src=/Users/hopescheffert/Documents/COMS319/WarGame/cards/back.gif";
-		s += " height=102 width=73 align='left'";
-		s += ">";
+		s += " src=/Users/hopescheffert/Documents/COMS319/WarGame/cards/" + element.src.toString() + ".gif";
+		s += " height=102 width=73";
+		s += " >";
 		return s;
 
 	},
@@ -379,23 +344,33 @@ var War =
 		return s;
 
 	},
-	//displaying a div element: SCORE
-	// displayDivElement : function (element, playerHand, computerHand)
-	// {
-	// 	var s = "<div ";
-	// 	s += " id=\"" + element.id;
-	// 	s += " value=Your Score: \"" + this.playerHand.length + " cards <br>" +
-	// 								"Computer Score:  " + this.computerHand.length + " cards <br>" + element.value + "\"";
-	// 	s += "</div>";
-	// 	return s;
-	//
-	// },
+	//displaying buttons
+	displayWarButton : function (element)
+	{
+		var s = "<input ";
+		s += " id=\"" + element.id + "\"";
+		s += " type=\"" + element.type + "\"";
+		s += " value= \"" + element.value + "\"";
+		s += " onclick= \"" + element.onclick + "\"";
+		s += "hidden>";
+		return s;
 
+	},
+
+	displayDivElement : function (message)
+	{
+		var s = "<br><div id='status'";
+		s += " <h5><b>" + message + "<b></h5>";
+		s += " </div><br>";
+		return s;
+
+	},
 	attachHandlers : function()
 	{
-		War.View["startButton"].onclick ="War.Controller.startButtonHandler()";
 		War.View["quitButton"].onclick ="War.Controller.quitButtonHandler()";
-		War.View["takeTurnButton"].onclick ="War.Controller.takeTurnButtonHandler(playerHand, computerHand)";
+		War.View["layDownACard"].onclick ="War.Controller.layDownACardHandler(playerHand, computerHand, false)";
+		War.View["warLayDownButton"].onclick = "War.Controller.warLayDownButtonHandler(playerHand, "
+			+ "playerLayUp, computerHand, computerLayUp, cardsInPlay)";
 	}
 
 
