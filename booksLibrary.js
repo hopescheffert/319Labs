26cff25e162ee2d3d36e060f936fbd0989db1ddd
hopescheffert <hopees@iastate.edu>
2016-10-06 16:08:31 -0500

    progress with library table

--- booksLibrary.js
+++ booksLibrary.js
@@ -11,41 +11,48 @@ function Library()
     this.artShelf = new Shelf("art");
     this.sportsShelf = new Shelf("sports");
     this.scienceShelf = new Shelf("science");
-    console.log("science shelf " + this.scienceShelf.booksOnShelf);
     this.literatureShelf = new Shelf("literature");
-    this.allBooks = [new Book(1, "ordinary", this), new Book(2, "ordinary", this), new Book(3, "reference", this), new Book(4, "ordinary", this)]; //put some books in for testing
-    //construct new shelves that start empty
+    this.allBooks = [];
+    //initialize library with 25 books (20 ordinary and 5 reference)
+    for(var i = 0; i < 21; i ++)
+    {
+        this.allBooks.push(new Book(i, "ordinary", this));
+    }
+    for(var i = 21; i < 26; i++)
+    {
+        this.allBooks.push(new Book(i, "reference", this));
+    }
 
 }
 
 //called to log in and show the library table in the html page
 Library.prototype.logIn = function(username, password)
 {
-    console.log("in log in")
     var username = username.value;
     var password = password.value;
 
     if(username == "admin" && password == "admin")
     {
         //log in as a librarian
-        //var lib = new Library("librarian");
-        Library.userType = "librarian";
-        console.log("lib is " + lib.allBooks);
+        var lib = new Library();
+        lib.userType = "librarian";
+        //console.log("lib is " + lib.allBooks);
 
     }
     else if(username.startsWith("u",0))
     {
         //log in as an undergrad student
         var lib = new Library();
-        Library.userType = "undergrad";
-        console.log("library consists of " + lib.allBooks);
-        console.log("user type is now " + lib.userType);
-
+        lib.userType = "undergrad";
+        //console.log("library consists of " + lib.allBooks.toString());
     }
-
+    else {
+        alert("Incorrect username or password");
+    }
+    console.log("lib " + lib.allBooks);
     //display library as table in html
     //do we need to pass the library to this function?
-    //document.getElementById("myLibrary").innerHTML = Library.prototype.displayLibrary(lib);
+    document.getElementById("myLibrary").innerHTML = Library.prototype.displayLibrary(lib);
 
 }
 
@@ -61,38 +68,66 @@ Library.prototype.createLoginPage = function()
 
     return s;
 }
-//TODO display the library as a table
+//display the library as a table
+//TODO note that this is only for these specific books and book id's
 Library.prototype.displayLibrary = function(lib)
 {
-    //insertRow()? http://www.w3schools.com/jsref/met_table_insertrow.asp
-    //each column is a Shelf
-    //each cell is a book (insertCell()?) http://www.w3schools.com/jsref/met_tablerow_insertcell.asp
+    //each column is a Shelf, each cell is a book on that shelf
+    console.log("literature shelf is : " + lib.literatureShelf.booksOnShelf);
+    console.log("science shelf is : " + lib.scienceShelf.booksOnShelf);
+    console.log("sports shelf is : " + lib.sportsShelf.booksOnShelf);
+    console.log("art shelf is : " + lib.artShelf.booksOnShelf);
+    console.log("lengths are " + lib.literatureShelf.booksOnShelf.length  + " " +
+    lib.scienceShelf.booksOnShelf.length + " " + lib.sportsShelf.booksOnShelf.length + " " +
+    lib.artShelf.booksOnShelf.length);
+
     var s;
     s = "<table id=\"myTable\" border='1px solid black'>";
-    s += "<tr><td>";
+    s += "<th style='background-color:green'>Literature Shelf</th>";
+    s += "<th style='background-color:green'>Science Shelf</th>";
+    s += "<th style='background-color:green'>Sports Shelf</th>";
+    s += "<th style='background-color:green'>Art Shelf</th>";
+    for(var i = 0; i < 6; i++)
+    {
+        s += "<tr id='row" + i +"'>";
+        if(lib.literatureShelf.booksOnShelf[i] != undefined)
+        {
+            s += "<td>";
+            s += Library.prototype.displayBook(lib.literatureShelf.booksOnShelf[i]);
+            s += "</td>";
+        }
 
-    console.log("books on shelf are " + artShelf.booksOnShelf);
-    console.log("books in library are " + lib.allBooks);
-    //not right but something like this?
-    // for(var i = 0; i < Libary.literatureShelf.booksOnShelf.length; i++)
-    // {
-    //     s += Library.prototype.displayBook(Library.literatureShelf.booksOnShelf[i]);
-    // }
-    // s += Library.scienceShelf.booksOnShelf;
-    // s += Library.sportsShelf.booksOnShelf;
-    // s += Library.artShelf.booksOnShelf;
+        if(lib.scienceShelf.booksOnShelf[i] != undefined)
+        {
+            s += "<td>";
+            s += Library.prototype.displayBook(lib.scienceShelf.booksOnShelf[i]);
+            s += "</td>";
+        }
 
+        if(lib.sportsShelf.booksOnShelf[i] != undefined)
+        {
+            s += "<td>";
+            s += Library.prototype.displayBook(lib.sportsShelf.booksOnShelf[i]);
+            s += "</td>";
+        }
 
-    // for(var i = 0; i < lib.allBooks.length; i ++)
-    // {
-    //     for(var j = 0; j < Shelf.booksOnShelf.length; j++)
-    //     {
-    //         s += Library.prototype.displayBook(Shelf.booksOnShelf[i]);
-    //         s += "</td><td>";
-    //     }
-    //
-    // }
-    s += "</td></tr>";
+        if(lib.artShelf.booksOnShelf[i] != undefined)
+        {
+            s += "<td>";
+            s += Library.prototype.displayBook(lib.artShelf.booksOnShelf[i]);
+            s += "</td>";
+        }
+        s+= "</tr>";
+
+    }
+    s += "<tr>";
+    s += "<td></td><td>";
+    s += Library.prototype.displayBook(lib.scienceShelf.booksOnShelf[6]);
+    s += "</td>";
+    s += "<td></td><td>";
+    s += Library.prototype.displayBook(lib.artShelf.booksOnShelf[6]);
+    s += "</td>";
+    s += "</tr>";
     s += "</table>";
     return s;
 }
@@ -102,8 +137,8 @@ Library.prototype.displayBook = function(bookObj)
     var s = "<input ";
     s += " id=\"" + bookObj.id + "\"";
     s += " type='text'";
-    s += " value= \"" + bookObj.id + "\"";
-    s += " onclick= \"" + bookObj.onclick + "\"";
+    s += " value= \"" + bookObj.toString() + "\"";
+    s += " onclick= \"" + bookObj.prototype.click(this) + "\"";
     s += ">";
     return s;
 }
@@ -122,6 +157,10 @@ function Book(bookid, bookType, library)
     //presence(1) or borrowed(0) situation of book
     this.presence = localStorage.setItem("presence", 1); //all books start with presence (1)
 }
+Book.prototype.click = function(that)
+{
+    alert("This book: " + that.toString());
+}
 
 //add the book to the correct shelf based on category
 //sets category attribute for Book object
@@ -135,8 +174,6 @@ Book.prototype.putOnShelf = function(book, library)
     else if(book.id % 4 == 1)
     {
         book.category = "science";
-        //TODO Library.scienceShelf is undefined! Why can't we grab this instance of library...
-        console.log("scienceShelf "+ Library.scienceShelf);
         library.scienceShelf.booksOnShelf.push(book);
 
     }
@@ -154,11 +191,31 @@ Book.prototype.putOnShelf = function(book, library)
     }
     //console.log("categorized book as " + book.category);
 }
-
+Book.prototype.toString = function()
+{
+    //return "[Book id: " + this.id + ", Book type: " + this.type + "]";
+    if(this.type == "ordinary")
+    {
+        return "B" + this.id;
+    }
+    else {
+        return "R" + this.id;
+    }
+}
 //shelf constructor
 function Shelf(category)
 {
     this.category = category; //represents the shelf category
     this.booksOnShelf = []; //represents all the books on this shelf
     //**different shelf for each category : art, science, sport, literature
+}
+Shelf.prototype.toString = function()
+{
+    var ret = "{";
+    for(var i = 0; i < this.booksOnShelf.length; i++)
+    {
+        ret += this.booksOnShelf[i].toString();
+    }
+    ret += "}";
+    return ret;
 }
