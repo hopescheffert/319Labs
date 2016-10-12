62eca0fd012c3df142a76a56545c2fcef7a5e57f
Hope Scheffert <hope_scheffert@domain.com>
2016-10-12 17:20:49 -0500

    working on checkLogin and inInbox

--- Lab5/checkLogin.php
+++ Lab5/checkLogin.php
@@ -9,23 +9,26 @@ $userFile = fopen("users.txt", "r") or die("Unable to open users.txt");
 fread($userFile, filesize("users.txt"));
 
 //get whole line from userFile
-$user = fgets($userFile);
-//TODO PARSE THE STRING TO GET  users/passwords/public key/private key
-//EX: smitra:testPassword:ddhfh9fi3f3i:94hredbb
-
-for($i = 0; $i < strlen($user); $i++)
-{
-    //charat $user
-    $char = $user{$i};
-    if(strcmp($char, ":") == 0)
-    {
-
-    }
-}
+$user = fgets($userFile); //TODO THIS MUST BE JSON ENCODED ARRAY (in signup.html)
+//decode the line from the users.txt file into an object so that we can get info
+$obj = json_decode($user);
+//get username and password ....do some check?
+$username = $obj->{'username'};
+$password = $obj->{'password'};
 
 
+$result = 0; //0 if failed or 1 if succeeded
+//TODO check username/password?
+//if(they are okay) $result = s
+//else $result = 0
+$jsonResponse = {'success(1)/failure(0)' : $result}; //json object to return with success/failure info
 
 //close the file
 fclose($userFile);
+
+
+return $jsonResponse;
+
+
 
  ?>
--- Lab5/inInbox.php
+++ Lab5/inInbox.php
@@ -1,6 +1,26 @@
 <!-- inInbox.php
-- use file_get_contents to get json data from messages.txt. Next, convert json to php
-object (json_decode/json_encode)
-- loop thru object: If the To part is related to the current username create html rows with
-after decrypting the body of the message.
--->
+- use file_get_contents to get json data from messages.txt.
+- Next, convert json to php object (json_decode/json_encode)
+- loop thru object:
+    If the To part is related to the current username create html rows with after decrypting the body of the message. -->
+
+
+<?php
+//$jsonData = file_get_contents("messages.txt");
+//example User:Alice; Receiver: BOB; Body: dshgkfjsghfjksghjkf
+
+$jsonData = array('User'=> "Alice", 'Reciever'=> "Bob", 'Body'=> "some text here");
+$jsonObj = json_encode($jsonData);
+$phpObject = json_decode($jsonObj);
+
+// echo "json object " . $jsonObj;
+// echo "php object ";
+// print_r($phpObject);
+
+//loop through object
+//If the To part is related to the current username create html rows with after decrypting the body of the message
+
+
+
+
+ ?>
