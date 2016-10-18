8153bfbf3fee868b676995d466dab9021fd06f32
Hope Scheffert <hope_scheffert@domain.com>
2016-10-18 13:58:20 -0500

    signup basically done

--- Lab5/checkLogin.php
+++ Lab5/checkLogin.php
@@ -3,10 +3,11 @@
 // read the users.txt file and then check the user/password.
 // return a json object with the success/failure info
 session_start();
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 
 
 $userFile = fopen("users.txt", "r") or die("Unable to open users.txt");
+
 
 // $response = new stdClass();
 // $response->success = true;
@@ -18,38 +19,41 @@ $failed = false;
 $curUser = $_REQUEST["username"];
 // echo $curUser . "<br>";
 $curPass = $_REQUEST["password"];
-// echo $curPass;
+// echo $curPass . "<br>";
+
 while(!feof($userFile))
 {
     //get whole line from userFile
-    $user = fgets($userFile); //THIS MUST BE JSON ENCODED (in signup.html)
-
+    //$user = fgets($userFile); //THIS MUST BE JSON ENCODED (in signup.html)
+    $user = stream_get_line($userFile, NULL, "***");
     //decode the line from the users.txt file into an object so that we can get info
+    echo $user . "<br>";
     $obj = json_decode($user);
     if($obj == null)
     {
         //reached end of file
         // $response->success = false;
-        echo json_encode($failed);
+        echo "failed because object is null <br>" . json_encode($failed) ."<br>";
         break;
     }
     //get username and password ....do some check?
     $username = $obj->username;
-    // echo "username is " . $username . "<br>";
+    echo "username is " . $username . "<br>";
 
     $password = $obj->password;
-    // echo "password is ".$password. "<br>";
+    echo "password is ".$password. "<br>";
 
 
     if((strcmp($username, $curUser) == 0)  && (strcmp($password, $curPass) == 0))
     {
         // $response->success = true;
         $_SESSION["username"] = $username;
-        echo json_encode($succeeded);
+        $_SESSION["password"] = $password;
+        echo "succeeded! " . json_encode($succeeded);
         break;
     }
 }
-json_encode($failed);
+// echo "failed because couldn't find in user file <br>" . json_encode($failed);
 
 //close the file
 fclose($userFile);
--- Lab5/signup.php
+++ Lab5/signup.php
@@ -1,43 +1,59 @@
 <?php
+session_start();
 // signup.php
 //does the encryption and decryption
 $path = 'phpseclib';
 	set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 	include_once('Crypt/RSA.php');
 
-//Function for encrypting with RSA
-function rsa_encrypt($string, $public_key)
-{
-    //Create an instance of the RSA cypher and load the key into it
-    $cipher = new Crypt_RSA();
-    $cipher->loadKey($public_key);
-    //Set the encryption mode
-    $cipher->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
-    //Return the encrypted version
-    return $cipher->encrypt($string);
-}
+$curUser = $_REQUEST['username'];
+$curPass = $_REQUEST['password'];
 
-//Function for decrypting with RSA
-function rsa_decrypt($string, $private_key)
-{
-    //Create an instance of the RSA cypher and load the key into it
-    $cipher = new Crypt_RSA();
-    $cipher->loadKey($private_key);
-    //Set the encryption mode
-    $cipher->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
-    //Return the decrypted version
-    return $cipher->decrypt($string);
-}
+$_SESSION['username'] = $curUser;
+$_SESSION['password'] = $curPass;
+
+
+// //Function for encrypting with RSA
+// function rsa_encrypt($string, $public_key)
+// {
+//     //Create an instance of the RSA cypher and load the key into it
+//     $cipher = new Crypt_RSA();
+//     $cipher->loadKey($public_key);
+//     //Set the encryption mode
+//     $cipher->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+//     //Return the encrypted version
+//     return $cipher->encrypt($string);
+// }
+//
+// //Function for decrypting with RSA
+// function rsa_decrypt($string, $private_key)
+// {
+//     //Create an instance of the RSA cypher and load the key into it
+//     $cipher = new Crypt_RSA();
+//     $cipher->loadKey($private_key);
+//     //Set the encryption mode
+//     $cipher->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+//     //Return the decrypted version
+//     return $cipher->decrypt($string);
+// }
 
 	$rsa = new Crypt_RSA();
 	$rsa->setPrivateKeyFormat(CRYPT_RSA_PRIVATE_FORMAT_PKCS1);
 	$rsa->setPublicKeyFormat(CRYPT_RSA_PUBLIC_FORMAT_PKCS1);
 	extract($rsa->createKey(1024)); /// makes $publickey and $privatekey available
-	echo $privatekey;
-	echo $publickey;
+	//echo "<script> var privatekey = " . $privatekey . "</script>";
+	//echo "<script> var publickey = " . $publickey . "</script>";
 //Private key
 $private_key = $privatekey;
 $public_key = $publickey;
+echo "private " . $private_key . " ";
+echo "public " . $public_key;
+
+//$user = '{"username" : ' . $curUser . ', "password" :' . $curPass . ', "publickey" : ' . $public_key . ', "privatekey" : ' . $private_key . '}\n';
+$user = array('username' => $curUser, 'password' => $curPass, 'publickey' => $public_key, 'privatekey' => $private_key);
+$entry = json_encode($user) . "***";
+file_put_contents("users.txt", $entry, FILE_APPEND);
+
 
 //Test out the rsa encryption functions
 // $plaintext = "This is some plaintext to encrypt";
