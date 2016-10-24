c5d7c334c5ea8c44d1868776299ebc3137747a43
Hope Scheffert <hope_scheffert@domain.com>
2016-10-23 19:34:52 -0500

    send messages

--- Lab5/sendmessage.php
+++ Lab5/sendmessage.php
@@ -1,35 +1,133 @@
 <?php
 session_start();
-// sendmessage.php
-// - Create a simple UI with a text file for Receiver part, User(fill with current username) and
-// Body part, with submit button.
-// -TODO  When User clicks on submit, the message should be encrypted by Receiver PublicKey
-// and store information (contain Sender username, Receiver and body of message) in
-// messages.txt file. Each item should contain the information as in this example
-//User:Alice; Receiver: BOB; Body: dshgkfjsghfjksghjkf
-//$jsonData = array('User'=> "Alice", 'Reciever'=> "Bob", 'Body'=> "dshgkfjsghfjksghjkf");
+$path = 'phpseclib';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+include_once('Crypt/RSA.php');
 
-// - Make sure that you store the encrypted message in messages.txt file.
-
-// TODO Each Message should contain at least a Sender, Receiver, and Body part
-// Use only AJAX to get/create Message
 $curUser = $_SESSION['username'];
-
-echo "Reciever: <br><input type='text' id='reciever'</input> <br>";
-echo "User: <br> <input type='text' id='user' value='".$curUser."' </input> <br>";
-echo "Body:<br> <textarea rows='4' cols='50'>Type your message here </textarea> <br>";
-echo "<input type='submit'</input>";
-//TODO $post = {"User": xxxx, "Reciever":xxxx , "Body": xxxxx, "Time" : xxxxx};
-
-//script for submit button
-echo "<script>";
-echo "document.getElementById('submit').onclick=function(){";
-echo "document.getElementById('reciever')";
-//TODO onclick should encrypt the message with Reciever's public key and store
-//user, reciever, body in messages.txt
-echo "});";
-
-echo "</script>";
+$user = $_REQUEST['user'];
+$reciever = $_REQUEST['reciever'];
+$body = $_REQUEST['body'];
+?>
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
 
 
- ?>
+<body>
+    <!-- sendmessage.php -->
+
+    <form id='myForm'>
+        Reciever: <br><input type='text' id='reciever'</input> <br>
+        User: <br> <input type='text' id='user' value="<?php echo $curUser ?>" </input> <br>
+        Body:<br> <textarea rows='4' id='body' cols='50'>Type your message here </textarea> <br>
+        <input id='submit' type='submit'</input>
+    </form>
+
+
+    <script>
+    $(document).ready(function()
+    {
+        var myform = $("#myform");
+        var user = $("#user");
+        var reciever = $("#reciever");
+        var body = $("#body");
+
+        myform.submit = function(e)
+        {
+            e.preventDefault(); //use ajax instead
+            $.get("sendmessagestuff.php?user=" + user.val() + "&body=" + body.val() + "&reciever=" + reciever.val(),
+            function(data)
+            {
+                alert("Message sent");
+                //var result = JSON.parse(data);
+            })
+        }
+    });
+    </script>
+
+
+
+</body>
+
+
+</html>
+
+
+<?php
+//Function for encrypting with RSA
+function rsa_encrypt($string, $public_key)
+{
+    //Create an instance of the RSA cypher and load the key into it
+    $cipher = new Crypt_RSA();
+    $cipher->loadKey($public_key);
+    //Set the encryption mode
+    $cipher->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+    //Return the encrypted version
+    return $cipher->encrypt($string);
+}
+
+//Function for decrypting with RSA
+function rsa_decrypt($string, $private_key)
+{
+    //Create an instance of the RSA cypher and load the key into it
+    $cipher = new Crypt_RSA();
+    $cipher->loadKey($private_key);
+    //Set the encryption mode
+    $cipher->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+    //Return the decrypted version
+    return $cipher->decrypt($string);
+}
+
+
+$rsa = new Crypt_RSA();
+$rsa->setPrivateKeyFormat(CRYPT_RSA_PRIVATE_FORMAT_PKCS1);
+$rsa->setPublicKeyFormat(CRYPT_RSA_PUBLIC_FORMAT_PKCS1);
+extract($rsa->createKey(1024)); /// makes $publickey and $privatekey available
+//$private_key = $privatekey;
+//$public_key = $publickey;
+
+$userFile = fopen("users.txt", "r") or die("Unable to open users.txt");
+
+while(!feof($userFile))
+{
+    //get whole line from userFile
+    //$user = fgets($userFile); //THIS MUST BE JSON ENCODED (in signup.html)
+    $userline = stream_get_line($userFile, NULL, "***\n");
+    //decode the line from the users.txt file into an object so that we can get info
+    //echo $user . "<br>";
+    $obj = json_decode($userline);
+    if($obj != null)
+    {
+
+        //get username and password
+        $username = $obj->username;
+
+        if((strcmp($username, $user) == 0))
+        {
+            $public_key = $obj->publickey;
+            break;
+        }
+        else if((strcmp($username, $reciever) == 0))
+        {
+            $private_key = $obj->privatekey;
+            break;
+        }
+    }
+}
+echo " private key is " . $private_key ." <BR>";
+echo " public key is " . $public_key ." <BR>";
+
+$ciphertext = rsa_encrypt($body, $public_key);
+$decipheredtext = rsa_decrypt($ciphertext, $private_key);
+
+echo "<br> encrypted text " . $ciphertext;
+echo "<br> decrypted text " . $decipheredtext;
+
+//$messagesFile = fopen("messages.txt", "r") or die("Unable to open messagesFile.txt");
+$message = array('user' => $curUser, 'reciever' => $reciever, 'body' => $ciphertext);
+$entry = json_encode($message) . "\n";
+file_put_contents("messages.txt", $entry, FILE_APPEND);
+
+?>
