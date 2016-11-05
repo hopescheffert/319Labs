a28e787d488efd1b2e792825ff62aa8d0c946abb
Jacob Neyens <jneyens@iastate.edu>
2016-11-04 19:48:17 -0500

    borrow and return book

--- Lab6/Book.php
+++ Lab6/Book.php
@@ -5,29 +5,44 @@ include("databaseconnection.php");
 
 if(($_GET['function']) == 'add')
 {
-    //adding book
-    echo "adding<br>";
-    $bookID = $_GET["bookID"];
-    $bookTitle = $_GET["bookTitle"];
-    $bookAuthor = $_GET["bookAuthor"];
-    $bookShelf = $_GET["bookShelf"];
-    $bookObj = new Book($bookID);//, $bookTitle, $bookAuthor, $bookShelf);
-    $bookObj->addBook($bookID, $bookTitle, $bookAuthor, $bookShelf);
+  //adding book
+  echo "adding<br>";
+  $bookID = $_GET["bookID"];
+  $bookTitle = $_GET["bookTitle"];
+  $bookAuthor = $_GET["bookAuthor"];
+  $bookShelf = $_GET["bookShelf"];
+  $bookObj = new Book($bookID);//, $bookTitle, $bookAuthor, $bookShelf);
+  $bookObj->addBook($bookID, $bookTitle, $bookAuthor, $bookShelf);
 
 }
 else if(($_GET['function']) == 'delete')
 {
-    echo "deleting<br>";
-    $bookID = $_GET["bookID"];
-    $bookObj = new Book($bookID);//, $bookTitle, $bookAuthor, $bookShelf);
-    $bookObj->deleteBook($bookID);
+  echo "deleting<br>";
+  $bookID = $_GET["bookID"];
+  $bookObj = new Book($bookID);//, $bookTitle, $bookAuthor, $bookShelf);
+  $bookObj->deleteBook($bookID);
 
 }
 else if(($_GET['function']) == 'viewShelves')
 {
-    $bookID = $_GET["bookID"];
-    $bookObj = new Book($bookID);//, $bookTitle, $bookAuthor, $bookShelf);
-    $bookObj->viewShelves($bookID);
+  echo "viewing shelves<BR>";
+  $bookID = $_GET["bookID"];
+  $bookObj = new Book($bookID);//, $bookTitle, $bookAuthor, $bookShelf);
+  $bookObj->viewShelves($bookID);
+}
+else if(($_GET['function']) == 'borrow')
+{
+  echo "borrowing book<BR>";
+  $bookID = $_GET["bookID"];
+  $bookObj = new Book($bookID);
+  $bookObj->borrowBook($bookID);
+}
+else if(($_GET['function']) == 'return')
+{
+  echo "returning book \n";
+  $bookID = $_GET["bookID"];
+  $bookObj = new Book($bookID);
+  $bookObj->returnBook($bookID);
 }
 
 
@@ -39,119 +54,149 @@ else if(($_GET['function']) == 'viewShelves')
 class Book
 {
 
-    public function __construct($bookID)//, $bookTitle, $bookAuthor, $bookShelf)
+  public function __construct($bookID)//, $bookTitle, $bookAuthor, $bookShelf)
+  {
+    $this->$bookID = $bookID;
+    // $this->$bookTitle = $bookTitle;
+    // $this->$bookAuthor = $bookAuthor;
+    // $this->$bookShelf = $bookShelf;
+  }
+
+  public function addBook($bookID, $bookTitle, $bookAuthor, $bookShelf) //librarian only
+  {
+    global $conn;
+
+    //adds to a shelf in the library that is not full
+    //Assume shelves have capacity of 20 books and that there are 4 shelves
+    //which names by “Art”, “Science”, “Sport” and “Literature”
+    if(strcmp($bookShelf, "literature") == 0)
     {
-        $this->$bookID = $bookID;
-        // $this->$bookTitle = $bookTitle;
-        // $this->$bookAuthor = $bookAuthor;
-        // $this->$bookShelf = $bookShelf;
+      $shelfID = 0;
+    }
+    else if(strcmp($bookShelf, "science") == 0)
+    {
+      $shelfID = 1;
+    }
+    else if(strcmp($bookShelf, "sports") == 0)
+    {
+      $shelfID = 2;
+    }
+    else if(strcmp($bookShelf, "art") == 0)
+    {
+      $shelfID = 3;
     }
 
-    public function addBook($bookID, $bookTitle, $bookAuthor, $bookShelf) //librarian only
+    //assume availability is 1 (present)
+    //add book to books table
+    $sql1 = "INSERT INTO books (bookID, bookTitle, author, availability) VALUES ('" . $bookID . "', '" . $bookTitle . "', '" . $bookAuthor . "', '1');";
+    //also add to shelves table
+
+    $sql1 .= "INSERT INTO shelfBooks (shelfID, bookID) VALUES ('" .$shelfID . "', '" . $bookID . "');";
+    $sql1 .= "INSERT INTO shelves (shelfID, shelfName) VALUES ('" .$shelfID . "', '" . $bookShelf . "')";
+    if(mysqli_multi_query($conn, $sql1) === FALSE)
     {
-        global $conn;
-
-        //adds to a shelf in the library that is not full
-        //Assume shelves have capacity of 20 books and that there are 4 shelves
-        //which names by “Art”, “Science”, “Sport” and “Literature”
-        if(strcmp($bookShelf, "literature") == 0)
-        {
-            $shelfID = 0;
-        }
-        else if(strcmp($bookShelf, "science") == 0)
-        {
-            $shelfID = 1;
-        }
-        else if(strcmp($bookShelf, "sports") == 0)
-        {
-            $shelfID = 2;
-        }
-        else if(strcmp($bookShelf, "art") == 0)
-        {
-            $shelfID = 3;
-        }
-
-        //assume availability is 1 (present)
-        //add book to books table
-        $sql1 = "INSERT INTO books (bookID, bookTitle, author, availability) VALUES ('" . $bookID . "', '" . $bookTitle . "', '" . $bookAuthor . "', '1');";
-        //also add to shelves table
-
-        $sql1 .= "INSERT INTO shelfBooks (shelfID, bookID) VALUES ('" .$shelfID . "', '" . $bookID . "');";
-        $sql1 .= "INSERT INTO shelves (shelfID, shelfName) VALUES ('" .$shelfID . "', '" . $bookShelf . "')";
-        if(mysqli_multi_query($conn, $sql1) === FALSE)
-        {
-            echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
-        }
-        else
-        {
-            while(mysqli_next_result($conn))
-            {
-                if(mysqli_more_results($conn))
-                {
-                    printf("________");
-                }
-            }
-        }
-        echo "done adding<BR>";
+      echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
     }
-
-    public function deleteBook($bookID) //librarian only
+    else
     {
-        //TODO check
-        global $conn;
-
-        $sql = "DELETE FROM books WHERE bookID='" . $bookID . "'";
-        if(mysqli_query($conn, $sql) === FALSE)
+      while(mysqli_next_result($conn))
+      {
+        if(mysqli_more_results($conn))
         {
-            echo "Error deleting record: " . mysqli_error($conn);
+          printf("________");
         }
-        //removes the book from the library
-        //Hint : sql delete from book table in DATABASE.
+      }
     }
+    echo "done adding<BR>";
+  }
 
-    public function viewShelves($bookID)
+  public function deleteBook($bookID) //librarian only
+  {
+    //TODO check
+    global $conn;
+
+    $sql = "DELETE FROM books WHERE bookID='" . $bookID . "'";
+    if(mysqli_query($conn, $sql) === FALSE)
     {
-        //TODO Use sql join between shelves and books and show information.
-        global $conn;
-        $sql = "SELECT books.bookID, books.bookTitle, books.author, books.availability, shelfBooks.shelfID FROM books INNER JOIN shelfBooks ON books.bookID=shelfBooks.bookID";
-        $result = mysqli_query($conn, $sql);
-        if(mysqli_num_rows($result) > 0)
-        {
-            // output data of each row
-            while($row = mysqli_fetch_assoc($result))
-            {
-                echo "Book ID: " . $row["bookID"]. " - Title: " . $row["bookTitle"]. " - Author: " . $row["author"]. " - Availability: " . $row["availability"]. " - Shelf ID: " . $row["shelfID"] . "\n";
-            }
-        }
-        else
-        {
-            echo "ERROR in viewShelves\n";
-        }
+      echo "Error deleting record: " . mysqli_error($conn);
     }
+    //removes the book from the library
+    //Hint : sql delete from book table in DATABASE.
+  }
 
-    function borrowBook($bookID) //student only
+  public function viewShelves($bookID)
+  {
+    //TODO Use sql join between shelves and books and show information.
+    global $conn;
+    $sql = "SELECT books.bookID, books.bookTitle, books.author, books.availability, shelfBooks.shelfID FROM books INNER JOIN shelfBooks ON books.bookID=shelfBooks.bookID";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0)
     {
-        //TODO check that it works
-        $sql = "UPDATE books SET availability = '0' WHERE bookID='" . $bookID . "'";
-        if(mysqli_query($conn, $sql) === FALSE)
-        {
-            echo "Error updating record: " . mysqli_error($conn);
-        }
-        //should be able to take a book on loan if it is available.
-        //Hint : Use sql update _books table.
+      // output data of each row
+      while($row = mysqli_fetch_assoc($result))
+      {
+        echo "Book ID: " . $row["bookID"]. " - Title: " . $row["bookTitle"]. " - Author: " . $row["author"]. " - Availability: " . $row["availability"]. " - Shelf ID: " . $row["shelfID"] . "\n";
+      }
     }
+    else
+    {
+      echo "ERROR in viewShelves\n";
+    }
+  }
 
-    function returnBook($bookID) //student only
+  function borrowBook($bookID) //student only
+  {
+    global $conn;
+    $sql2 = "SELECT availability FROM books WHERE bookID= '" . $bookID . "'";
+    $result = mysqli_query($conn, $sql2);
+    $availability = mysqli_fetch_assoc($result);
+    if($availability == 0)
     {
-        //TODO check that it works
-        $sql = "UPDATE books SET availability = '1' WHERE bookID='" . $bookID . "'";
-        if(mysqli_query($conn, $sql) === FALSE)
-        {
-            echo "Error updating record: " . mysqli_error($conn);
-        }
-        //return the book and make the book available.
-        //Hint : Use sql update_books table.
+      echo "can not borrow this book, it is already checked out \n";
     }
+    else
+    {
+      echo "book can be borrowed! \n";
+      $sql = "UPDATE books SET availability = '0' WHERE bookID='" . $bookID . "'";
+      if(mysqli_query($conn, $sql) === FALSE)
+      {
+        echo "Error updating record: " . mysqli_error($conn);
+      }
+      else
+      {
+        echo "borrowBook worked!\n";
+      }
+    }
+    //should be able to take a book on loan if it is available.
+    //Hint : Use sql update_books table.
+  }
+
+  function returnBook($bookID) //student only
+  {
+    global $conn;
+    $sql2 = "SELECT availability FROM books WHERE bookID= '" . $bookID . "'";
+    $result = mysqli_query($conn, $sql2);
+    $availability = mysqli_fetch_assoc($result);
+    if($availability == 1)
+    {
+      echo "can not return this book, it is already returned \n";
+    }
+    else
+    {
+      echo "book can be returned! \n";
+      $sql = "UPDATE books SET availability = '1' WHERE bookID='" . $bookID . "'";
+      if(mysqli_query($conn, $sql) === FALSE)
+      {
+        echo "Error updating record: " . mysqli_error($conn);
+      }
+      else
+      {
+        echo "returnBook worked!\n";
+      }
+    }
+    //return the book and make the book available.
+    //Hint : Use sql update_books table.
+  }
 
 
 
--- Lab6/Student.php
+++ Lab6/Student.php
@@ -4,90 +4,190 @@ session_start();
 require("databaseconnection.php");
 if(isset($_SESSION["username"]))
 {
-    $curUser = $_SESSION["username"];
-    echo "<div style='float:right' >Welcome, " . $curUser. "</div><br>";
-    echo "<div style='float:right'> <a href='logout.php'>Logout</a>". "</div><br>";
+  $curUser = $_SESSION["username"];
+  echo "<div style='float:right' >Welcome, " . $curUser. "</div><br>";
+  echo "<div style='float:right'> <a href='logout.php'>Logout</a>". "</div><br>";
 }
 ?>
 <html>
 <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
 </head>
 <body>
-    <div id="myLibrary">
-        <table id="lib" border='1px solid black'>
-            <th style='background-color:green'>Literature Shelf</th>
-            <th style='background-color:green'>Science Shelf</th>
-            <th style='background-color:green'>Sports Shelf</th>
-            <th style='background-color:green'>Art Shelf</th>
-        </table>
-    </div>
+  <div id="myLibrary">
+    <table id="lib" border='1px solid black'>
+      <th style='background-color:green'>Literature Shelf</th>
+      <th style='background-color:green'>Science Shelf</th>
+      <th style='background-color:green'>Sports Shelf</th>
+      <th style='background-color:green'>Art Shelf</th>
+    </table>
+  </div>
 
-    <!-- <input type="button" id="borrowBook" value="Borrow Book"</input>
-    <input type="button" id="returnBook" value="Return Book"</input> -->
+  <input type="button" id="borrowBook" value="Borrow Book"</input>
+  <input type="button" id="returnBook" value="Return Book"</input>
 
 
 
-    <script>
-    //TODO need the library to save books in the shelves
-    $(document).ready(function()
+  <script>
+  //TODO need the library to save books in the shelves
+  $(document).ready(function()
+  {
+    var borrowBookButton = $("#borrowBook");
+    var returnBookButton = $("#returnBook");
+    var table = document.getElementById("lib");
+
+    //init table - 20 emtpy rows
+    for(var i = 0; i < 20; i++)
     {
+      var row = table.insertRow(-1);
+      row.id = "row" + i;
+      for(var j = 0; j < 4; j++)
+      {
+        var cell = row.insertCell(-1);
+        cell.id = "row" + i + "col" + j;
+        cell.innerHTML = "";
+      }
+    }
 
-        //ajax request to display borrowed history
-        // var borrowBookButton = $("#borrowBook");
-        // var returnBookButton = $("#returnBook");
-        var table = document.getElementById("lib");
-
-        //init table - 20 empty rows
-        for(var i = 0; i < 20; i++)
+    //get the books from the database to put into table
+    $.get("LibraryOfBooks.php?getBooks=true", function(data)
+    {
+      //TODO get it from the database
+      //data is a json_array of bookID's
+      result = JSON.parse(data);
+      for(var i = 0; i < result.length; i++)
+      {
+        if(result[i].sid == 0)
         {
-            var row = table.insertRow(-1);
-            row.id = "row" + i;
-            for(var j = 0; j < 4; j++)
+          for(var j = 0; j < 20; j++)
+          {
+            r = document.getElementById("row"+j);
+            c = document.getElementById("row"+j+"col"+0);
+            if(c.innerHTML == "")
             {
-                var cell = row.insertCell(-1);
-                cell.id = "row" + i + "col" + j;
-                $.get("Student.php?getBook", function()
-                {
-                    //TODO get it from the database
-                    cell.innerHTML = data;
-                })
+              break;
             }
+          }
+          var row = document.getElementById("row" + j);
+          var cell = document.getElementById("row" + j + "col" + 0);
+          if(cell.innerHTML == "")
+          {
+            cell.innerHTML = result[i].bid;
+            cell.onclick = function()
+            {
+              $.get("Book.php?function=viewShelves&bookID=" + cell.innerHTML, function(data)
+              {
+                alert(data);
+
+              })
+            }
+          }
 
         }
-      })
+        else if(result[i].sid == 1)
+        {
+          for(var j = 0; j < 20; j++)
+          {
+            r = document.getElementById("row"+j);
+            c = document.getElementById("row"+j+"col"+1);
+            if(c.innerHTML == "")
+            {
+              break;
+            }
+          }
+          var row = document.getElementById("row" + j);
+          var cell = document.getElementById("row" + j + "col" + 1);
+          if(cell.innerHTML == "")
+          {
+            cell.innerHTML = result[i].bid;
+            cell.onclick = function()
+            {
+              $.get("Book.php?function=viewShelves&bookID=" + cell.innerHTML, function(data)
+              {
+                alert(data);
 
-      // clickAction.on("click", function()
-      // {
-      //
-      //
-      // })
+              })
+            }
+          }
+
+        }
+        else if(result[i].sid == 2)
+        {
+          for(var j = 0; j < 20; j++)
+          {
+            r = document.getElementById("row"+j);
+            c = document.getElementById("row"+j+"col"+2);
+            if(c.innerHTML == "")
+            {
+              break;
+            }
+          }
+          var row = document.getElementById("row" + j);
+          var cell = document.getElementById("row" + j + "col" + 2);
+          if(cell.innerHTML == "")
+          {
+            cell.innerHTML = result[i].bid;
+            cell.onclick = function()
+            {
+              $.get("Book.php?function=viewShelves&bookID=" + cell.innerHTML, function(data)
+              {
+                alert(data);
+
+              })
+            }
+          }
+
+        }
+        else if(result[i].sid == 3)
+        {
+          for(var j = 0; j < 20; j++)
+          {
+            r = document.getElementById("row"+j);
+            c = document.getElementById("row"+j+"col"+3);
+            if(c.innerHTML == "")
+            {
+              break;
+            }
+          }
+          var row = document.getElementById("row" + j);
+          var cell = document.getElementById("row" + j + "col" + 3);
+          if(cell.innerHTML == "")
+          {
+            cell.innerHTML = result[i].bid;
+            cell.onclick = function()
+            {
+              $.get("Book.php?function=viewShelves&bookID=" + cell.innerHTML, function(data)
+              {
+                alert(data);
+
+              })
+            }
+          }
+
+        }
+      }
 
 
-    </script>
+    })
 
-    <?php
-
-    function borrowBook()
+    borrowBookButton.on("click", function()
     {
+      bookID = prompt("Enter the book ID of the book you would like to borrow");
+      $.get("Book.php?function=borrow&bookID=" + bookID)
+    })
 
-    }
-
-    function returnBook()
+    returnBookButton.on("click", function()
     {
-
-    }
-
-    function getBook()
-    {
-        global $conn;
-
-    }
+      bookID = prompt("Enter the book ID of the book you would like to return");
+      $.get("Book.php?function=return&bookID=" + bookID)
+    })
+  })
 
 
-    ?>
 
+
+  </script>
 
 </body>
 
