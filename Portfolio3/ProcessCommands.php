55d2c69e1eda92f4a8b45897ea62e9220a31859f
Hope Scheffert <hope_scheffert@domain.com>
2016-12-04 19:40:45 -0600

    works with input on webpage

--- Portfolio3/ProcessCommands.php
+++ Portfolio3/ProcessCommands.php
@@ -4,7 +4,9 @@ $commands = $_REQUEST["commands"];
 
 //run the parser with commands!
 
-$parse = shell_exec("./runAntlr.sh DrawingBoard.in 2>&1");
+file_put_contents("commands.in", $commands);
+
+$parse = shell_exec("./runAntlr.sh commands.in 2>&1");
 echo $parse;
 
 
--- Portfolio3/runAntlr.sh
+++ Portfolio3/runAntlr.sh
@@ -1,1 +1,1 @@
-java -cp ".:./antlr-4.5.3-complete.jar" org.antlr.v4.gui.TestRig DrawingBoard start -tree < DrawingBoard.in
+java -cp ".:./antlr-4.5.3-complete.jar" org.antlr.v4.gui.TestRig DrawingBoard start -tree < $1
